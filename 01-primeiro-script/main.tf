8b50c62a1e08808704922769881865bf238d600f
pedro <pedrovteixeira98@gmail.com>
2022-11-28 22:17:10 -0300

    01 - Criando primeiro Bucket no S3

---
+++ 01-primeiro-script/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_version = "1.3.5"
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = " 4.41.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = "us-east-2"
+    profile = "pedro.santos"
+}
+
+resource "aws_s3_bucket" "my-test-bucket" {
+  bucket = "my-tf-test-bucket-pedro-santos"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+    Managedby = "Terraform"
+  }
+}
+
+resource "aws_s3_bucket_acl" "my-test-bucket-acl" {
+  bucket = aws_s3_bucket.my-test-bucket.id
+  acl    = "private"
+}
